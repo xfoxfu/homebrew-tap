5843418bc8ff7cc1a26ddc44558403ab2cbf39ab
Yuze Fu <i@xfox.me>
2023-11-11 19:23:08 +0900

    arctime-pro: fix style

--- Casks/arctime-pro.rb
+++ Casks/arctime-pro.rb
@@ -3,7 +3,7 @@ cask "arctime-pro" do
   sha256 "9d22e35be3462bb5784387f6b1e3224a1d3160ac60781083c832f922a4ee5122"
 
   url "https://dl.arctime.pro/ARCTIME_PRO_#{version}_MAC.zip",
-      verified: "dl.arctime.pro"
+      verified: "dl.arctime.pro/"
   name "arctime-pro"
   desc "Cross platform subtitle tool"
   homepage "https://arctime.cn/"
@@ -11,10 +11,11 @@ cask "arctime-pro" do
   livecheck do
     url "https://t.arctime.cn/ap2m"
     strategy :header_match
-  end  
+  end
+
+  auto_updates true
 
   app "ARCTIME_PRO_#{version}_MAC/ArcTime Pro.app"
-  auto_updates true
 
   caveats do
     depends_on_java
